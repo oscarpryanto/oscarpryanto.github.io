928a9ab8862965d3dccc4bc9b71b24fab7c64468
oscarpryanto <oscarpryanto@gmail.com>
2022-06-22 17:29:08 +0700

    Uploading all file

---
+++ script.js
@@ -0,0 +1,14 @@
+const klik = document.querySelector('.button');
+
+klik.addEventListener('click', function(){
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(response => {
+            document.querySelector(".head").innerHTML = `<h2>"${response.slip.advice}"</h2>`;
+        });     
+    
+        
+})
+
+
+
